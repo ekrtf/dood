d248365f1c2fffcc3dfd7f8e21bf60f7d17a753e
zceiekr <emile@kratiroff.com>
2016-11-13 13:22:50 +0000

    (feat) Yelp auth

--- server/sYelp/sYelp.js
+++ server/sYelp/sYelp.js
@@ -1,16 +1,13 @@
 /**
- * @module YelpAdapterService
+ * @module YelpService
  * @type adapter
  * @description entry point of yelp API calls
  */
 
-const Yelp = require('yelp');
-const config = require('../config.json');
+const co = require('co');
+const rp = require('request-promise');
 
-let yelp = null;
-let logger = null;
-
-module.exports = YelpAdapterService;
+module.exports = YelpService;
 
 /* * * * * * * * * *
  *
@@ -18,23 +15,29 @@ module.exports = YelpAdapterService;
  *
  * * * * * * * * * */
 
-function YelpAdapterService($logger) {
-    logger = $logger;
-    yelp = new Yelp({
-        consumer_key: config.apis.yelp.consumer_key,
-        consumer_secret: config.apis.yelp.consumer_secret,
-        token: config.apis.yelp.toker,
-        token_secret: config.apis.yelp.token_secret
-    });
+function YelpService($config) {
+    this.config = $config;
+    this.accessToken = null;
 }
 
 /* * * * * * * * * *
  *
- * Public functions
+ * Initialization
  *
  * * * * * * * * * */
 
-YelpAdapterService.prototype.searchYelp = function(query) {
+YelpService.prototype.$init = co.wrap(function*() {
+    this.auth = yield this._getAccessToken();
+    this.accessToken = this.auth.access_token;
+});
+
+/* * * * * * * * * *
+ *
+ * Public Functions
+ *
+ * * * * * * * * * */
+
+YelpService.prototype.searchYelp = function(query) {
     let terms;
     let categories;
     let location;
@@ -47,4 +50,27 @@ YelpAdapterService.prototype.searchYelp = function(query) {
     yelp.search(search).then(function(data) {
         return data;
     });
+};
+
+/* * * * * * * * * *
+ *
+ * Private Functions
+ *
+ * * * * * * * * * */
+
+YelpService.prototype._getAccessToken = function() {
+    const options = {
+        method: 'POST',
+        uri: 'https://api.yelp.com/oauth2/token',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            'grant_type': 'client_credentials',
+            'client_id': this.config.appId,
+            'client_secret': this.config.appSecret
+        }
+    };
+
+    return rp(options).catch(console.log);
 };
