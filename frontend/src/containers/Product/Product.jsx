ed32818256232265cabc9dc418eb7f91510868fe
zceiekr <emile@kratiroff.com>
2016-11-22 11:51:26 +0000

    (feat) product buttonbox

--- frontend/src/containers/Product/Product.jsx
+++ frontend/src/containers/Product/Product.jsx
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Lightbox from 'react-images';
 import { toggleImages, selectImage } from '../../actions/product.actions';
+import { Button } from 'react-bootstrap';
 
 class Product extends Component {
     constructor(props) {
         super(props);
+
+        this.doSelectProduct = this.doSelectProduct.bind(this);
+        this.doAddProductToList = this.doAddProductToList.bind(this);
+
         this._openLightbox = this._openLightbox.bind(this);
         this._goToPrevious = this._goToPrevious.bind(this);
         this._goToNext = this._goToNext.bind(this);
         this._onClose = this._onClose.bind(this);
+    }
+
+    doSelectProduct() {
+        // TODO
+    }
+
+    doAddProductToList() {
+        // TODO
     }
 
     _goToPrevious() {
@@ -36,27 +49,40 @@ class Product extends Component {
         const images = this.props.product.images;
         if (!images) return;
 
-		const gallery = images.map((obj, i) => {
+		const gallery = images.map((item, index) => {
 			return (
-				<a key={i} onClick={(e) => this._openLightbox(i, e)}>
-					<img className="product__gallery__image" src={obj.src} />
+				<a key={index} onClick={(e) => this._openLightbox(index, e)}>
+					<img className="product__gallery__image" src={item.src} />
 				</a>
 			);
 		});
 
 		return (
-			<div className="product__gallery">
-				{gallery}
-			</div>
+			<div className="product__gallery">{ gallery }</div>
 		);
     }
 
+    _renderCategories() {
+        const categories = this.props.product.categories.map(c => c.title);
+        const tags = categories.map((item, index) => {
+            return (
+                <div key={index}>{item}</div>
+            );
+        });
+
+        return (
+            <div className="product__categories">{ tags }</div>
+        );
+    }
+
     render() {
-        const { name } = this.props.product;
+        const { name, price } = this.props.product;
 
         return (
             <div className="product">
                 { name }
+                { this._renderCategories() }
+                { price }
                 { this._renderGallery() }
                 <Lightbox
                     images={this.props.product.images}
@@ -68,6 +94,10 @@ class Product extends Component {
                     enableKeyboardInput={true}
                     showThumbnails={true}
                 />
+                <div className="product__buttonbox">
+                    <Button className="button big" onClick={this.doAddProductToList}>Add to Shortlist</Button>
+                    <Button className="button special big" onClick={this.doSelectProduct}>Choose</Button>
+                </div>
             </div>
         );
     }
