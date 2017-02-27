5bcd79be354fbe4167e7c00b5b0009b80f51fc2d
zceiekr <emile@kratiroff.com>
2017-02-27 12:03:52 +0000

    (feat) Emojis

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -22,9 +22,15 @@ class Home extends Component {
             <div className="container home">
                 <div className="row home__content">
                     <div className="col col-md-6">
-                        <section>
-                            <h4>What is this disseration about?</h4>
+                        <section className="home__content__section">
+                            <h4 className="home__content__heading">What is this disseration about?</h4>
                             <p>
+                                Trying to find out if there is significant business value in enhancing your user 
+                                experience on travel websites (think Yelp) with machine learning techniques.
+                                Online businesses use it widely today to serve you ads for the product you just 
+                                bought and collect large amount of information about you.
+                            </p>
+                            <p className="home__content__section">
                                 Trying to find out if there is significant business value in enhancing your user 
                                 experience on travel websites (think Yelp) with machine learning techniques.
                                 Online businesses use it widely today to serve you ads for the product you just 
@@ -33,17 +39,16 @@ class Home extends Component {
                         </section>
                     </div>
                     <div className="col col-md-6">
-                        <section>
-                            <h4>Why this experiment?</h4>
+                        <section className="home__content__section">
+                            <h4 className="home__content__heading">Why this experiment?</h4>
                             <p>
-                                Trying to find out if there is significant business value in enhancing your user 
-                                experience on travel websites (think Yelp) with machine learning techniques.
-                                Online businesses use it widely today to serve you ads for the product you just 
-                                bought and collect large amount of information about you.
+                                To answer this question I need to collect data from real world users in 
+                                a real world situation. Measuring the performance of my proposed solution
+                                allows to draw conclusions about the relevance of such system.
                             </p>
                         </section>
-                        <section>
-                            <h4>Rules</h4>
+                        <section className="home__content__section">
+                            <h4 className="home__content__heading">Rules</h4>
                             <ol>
                                 <li>Spend the time to find an option when you would actually go</li>
                                 <li>Do click "Nothing's good enough" if what is how you feel</li>
@@ -54,11 +59,14 @@ class Home extends Component {
                 </div>
 
                 <div className="home__tldr">
-                    <h3>TL;DR: Please take part in the experiement</h3>
+                    <h4>TL;DR: Please take part in the experiement</h4>
                 </div>
 
                 <div className="home__start">
-                    <button onClick={(e) => this._handleGetStarted(e)}>Get Started</button>
+                    <button onClick={(e) => this._handleGetStarted(e)}>
+                        Get Started
+                        <i className="em em-rocket"></i>
+                    </button>
                 </div>
             </div>
         );
