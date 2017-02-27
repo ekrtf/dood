bacbc741faff9189de5f436ac05738ff739af3b1
zceiekr <emile@kratiroff.com>
2017-02-27 10:18:11 +0000

    (redesign) Header bar style

--- frontend/src/components/HeaderBar/HeaderBar.jsx
+++ frontend/src/components/HeaderBar/HeaderBar.jsx
@@ -7,17 +7,7 @@ class HeaderBar extends Component {
         return (
             <div className="headerbar">
                 <div className="headerbar__logo">
-                    <div className="headerbar__logo__text">
-                        <IndexLink to="/">Dissertation</IndexLink>
-                    </div>
-                </div>
-                <div className="headerbar__menu">
-                    <div className="headerbar__menu__item">
-                        <IndexLink to="/clone">Clone</IndexLink>
-                    </div>
-                    <div className="headerbar__menu__item">
-                        <IndexLink to="/ml">ML</IndexLink>
-                    </div>
+                    <IndexLink to="/">Dissertation</IndexLink>
                 </div>
             </div>
         );
