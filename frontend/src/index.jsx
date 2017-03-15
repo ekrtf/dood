5de50381a81c605fc7644f1ef4581418f9cf3370
zceiekr <emile@kratiroff.com>
2017-03-15 13:54:27 +0000

    (feat) Google Analytics

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
 import { Router, Route, IndexRedirect, IndexRoute, browserHistory } from 'react-router';
 import { App, About, Home, Smart, Clone, Product, Feedback, Finish } from './containers';
 import configureStore from './utils/configure-store';
@@ -10,9 +11,15 @@ import './index.html';
 
 const store = configureStore();
 
+ReactGA.initialize('UA-93789722-1');
+const logPageView = () => {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+};
+
 render(
     <Provider store={store}>
-        <Router history={browserHistory}>
+        <Router history={browserHistory} onUpdate={logPageView}>
             <Route path="/" component={App}>
                 <IndexRoute component={Home} />
                 <Route path="/clone" component={Clone} />
