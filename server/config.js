aa009b320347bd147bf981d4a7378343a4dd7e79
zceiekr <emile@kratiroff.com>
2017-03-19 14:38:17 +0000

    (debug) Try connect with pg driver

--- server/config.js
+++ server/config.js
@@ -16,6 +16,52 @@ const prodConfig = {
     }
 };
 
+var pg = require('pg');
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+var config = {
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    database : process.env.RDS_DB_NAME,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+console.log('\n\n\n\nPG CONFIG: ', config)
+
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+var pool = new pg.Pool(config);
+
+// to run a query we can acquire a client from the pool,
+// run a query on the client, and then return the client to the pool
+pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+
+  console.log('\n\n\n\n\n\n\n\nall good\n\n\n\n\n\n\n\n\n')
+  console.log('client')
+  done();
+});
+
+pool.on('error', function (err, client) {
+  // if an error is encountered by a client while it sits idle in the pool
+  // the pool itself will emit an error event with both the error and
+  // the client which emitted the original error
+  // this is a rare occurrence but can happen if there is a network partition
+  // between your application and the database, the database restarts, etc.
+  // and so you might want to handle it and at least log it out
+  console.error('idle client error', err.message, err.stack)
+})
+
 const devConfig = {
     "client": "sqlite",
     "connection": {
